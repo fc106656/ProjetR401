734742e61f2f0c6d570d09ef742dc2f3b426f21f
fc106656 <clement.faux@etu.unice.fr>
2023-03-16 12:07:17 +0100

    Update serveur_authentification.php

--- serveur_authentification.php
+++ serveur_authentification.php
@@ -23,7 +23,57 @@
     case "POST" :
         /// Récupération des données envoyées par le Client
         $postedData = file_get_contents('php://input');
+
         /// Traitement
+
+        // se connecter à la base de données MySQL
+        $servername = "localhost";
+        $username = "username";
+        $password = "password";
+        $dbname = "database";
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        // vérifier la connexion
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        // récupérer les données de l'article à partir de la requête HTTP
+        $titre = $_POST['titre'];
+        $contenu = $_POST['contenu'];
+        $id_utilisateur = $_POST['id_utilisateur'];
+
+        // vérifier si l'utilisateur existe dans la table "utilisateur"
+        $sql = "SELECT * FROM utilisateur WHERE id_utilisateur = '$id_utilisateur'";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) == 0) {
+        // si l'utilisateur n'existe pas, renvoyer une erreur
+        http_response_code(400);
+        echo "L'utilisateur n'existe pas";
+        } else {
+        // insérer le nouvel article dans la table "article"
+        $sql = "INSERT INTO article (titre, contenu, id_utilisateur_de_auteur) VALUES ('$titre', '$contenu', '$id_utilisateur')";
+        if (mysqli_query($conn, $sql)) {
+            // récupérer l'ID de l'article nouvellement créé
+            $id_article = mysqli_insert_id($conn);
+            // insérer une ligne dans la table "like_dislike" pour enregistrer le premier vote (like) pour l'article nouvellement créé
+            $sql = "INSERT INTO like_dislike (id_article, id_utilisateur) VALUES ('$id_article', '$id_utilisateur')";
+            mysqli_query($conn, $sql);
+            // renvoyer une réponse HTTP avec un code de statut "201 Created" et l'ID de l'article nouvellement créé dans la réponse
+            http_response_code(201);
+            echo $id_article;
+        } else {
+            // si l'insertion de l'article a échoué, renvoyer une erreur
+            http_response_code(500);
+            echo "Erreur : " . mysqli_error($conn);
+        }
+        }
+
+        // fermer la connexion à la base de données MySQL
+        mysqli_close($conn);
+
+
         /// Envoi de la réponse au Client
         deliver_response(201, "Votre message", NULL);
         break;
