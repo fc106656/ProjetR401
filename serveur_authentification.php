ebc89e3eaf2901be1f6de54adee3f1f2fbfc19e4
fc106656 <clement.faux@etu.unice.fr>
2023-03-16 11:47:48 +0100

    mise en forme fichiers serveurs

--- serveur_authentification.php
+++ serveur_authentification.php
@@ -7,54 +7,56 @@
 
  /// Identification du type de méthode HTTP envoyée par le client
  $http_method = $_SERVER['REQUEST_METHOD'];
+ 
  switch ($http_method){
- /// Cas de la méthode GET
- case "GET" :
- /// Récupération des critères de recherche envoyés par le Client
- if (!empty($_GET['mon_critere'])){
- /// Traitement
-}
- /// Envoi de la réponse au Client
-deliver_response(200, "Votre message", $matchingData);
- break;
- /// Cas de la méthode POST
- case "POST" :
- /// Récupération des données envoyées par le Client
- $postedData = file_get_contents('php://input');
+    /// Cas de la méthode GET
+    case "GET" :
+        /// Récupération des critères de recherche envoyés par le Client
+        if (!empty($_GET['mon_critere'])){
+            /// Traitement
+        }
+        /// Envoi de la réponse au Client
+        deliver_response(200, "Votre message", $matchingData);
+        break;
 
- /// Traitement
- /// Envoi de la réponse au Client
- deliver_response(201, "Votre message", NULL);
- break;
- /// Cas de la méthode PUT
- case "PUT" :
- /// Récupération des données envoyées par le Client
- $postedData = file_get_contents('php://input');
+    /// Cas de la méthode POST
+    case "POST" :
+        /// Récupération des données envoyées par le Client
+        $postedData = file_get_contents('php://input');
+        /// Traitement
+        /// Envoi de la réponse au Client
+        deliver_response(201, "Votre message", NULL);
+        break;
 
- /// Traitement
- /// Envoi de la réponse au Client
- deliver_response(200, "Votre message", NULL);
- break;
- /// Cas de la méthode DELETE
- default :
- /// Récupération de l'identifiant de la ressource envoyé par le Client
- if (!empty($_GET['mon_id'])){
- /// Traitement
-}
- /// Envoi de la réponse au Client
- deliver_response(200, "Votre message", NULL);
- break;
+    /// Cas de la méthode PUT
+    case "PUT" :
+        /// Récupération des données envoyées par le Client
+        $postedData = file_get_contents('php://input');
+        /// Traitement
+        /// Envoi de la réponse au Client
+        deliver_response(200, "Votre message", NULL);
+        break;
+    
+    /// Cas de la méthode DELETE
+    default :
+    /// Récupération de l'identifiant de la ressource envoyé par le Client
+        if (!empty($_GET['mon_id'])){
+            /// Traitement
+        }
+        /// Envoi de la réponse au Client
+        deliver_response(200, "Votre message", NULL);
+        break;
 }
 /// Envoi de la réponse au Client
 function deliver_response($status, $status_message, $data){
- /// Paramétrage de l'entête HTTP, suite
-header("HTTP/1.1 $status $status_message");
-/// Paramétrage de la réponse retournée
-$response['status'] = $status;
-$response['status_message'] = $status_message;
-$response['data'] = $data;
-/// Mapping de la réponse au format JSON
-$json_response = json_encode($response);
-echo $json_response;
+    /// Paramétrage de l'entête HTTP, suite
+    header("HTTP/1.1 $status $status_message");
+    /// Paramétrage de la réponse retournée
+    $response['status'] = $status;
+    $response['status_message'] = $status_message;
+    $response['data'] = $data;
+    /// Mapping de la réponse au format JSON
+    $json_response = json_encode($response);
+    echo $json_response;
 }
 ?>
