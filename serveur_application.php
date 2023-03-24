62587f4a9c661f0ed25bb8403de24cada0259b05
fc106656 <clement.faux@etu.unice.fr>
2023-03-24 15:52:23 +0100

    Finalisation de la methode GET

--- serveur_application.php
+++ serveur_application.php
@@ -20,52 +20,167 @@ require_once('config.php');
             $payload = get_jwt_payload($bearer);
             $payload_role = $payload['role_r'];
             $payload_id = $payload['id'];
-        
-            if (!empty($_GET['mon_critere'])){
-                // Requête SQL pour récupérer les articles avec leur ID, date, nom et prénom de l'auteur, et contenu
-                $sql = "SELECT a.id_article, a.date_d, a.contenu, u.nom, u.prenom
-                        FROM article a
-                        JOIN utilisateur u ON u.id_utilisateur = a.fk_id_auteur";
 
-                // Exécution de la requête
-                $result = $database->query($sql);
+            switch($payload_role){
+                case "admin":
+                    if (empty($_GET['mon_critere'])){
+                        // Requête SQL pour récupérer les articles avec leur ID, date, nom et prénom de l'auteur, et contenu
+                        $sql = "SELECT * FROM article";
+                        // Exécution de la requête
+                        $psql = $database->prepare($sql);
+                        $psql->execute();
+                        $result = $psql;
 
-                // Création d'un tableau pour stocker les articles
-                $articles = array();
-                // Récupération des données et ajout au tableau des articles
-                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    switch($payload_role){
-                        case "admin":
-                            $sql2 = "SELECT * FROM likedislike WHERE id_utilisateur = :id_utilisateur";
+                        // Création d'un tableau pour stocker les articles
+                        $articles = array();
+                        // Récupération des données et ajout au tableau des articles
+                        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                            echo $row["fk_id_auteur"];
+                            $reqauteur = "SELECT * FROM utilisateur WHERE id_utilisateur = :id_utilisateur";
+                            $auteur = $database->prepare($reqauteur);
+                            $params = array(
+                                ':id_utilisateur' => $row["fk_id_auteur"]
+                            );
+                            $auteur->execute($params);
+                            $auteur = $auteur->fetch(PDO::FETCH_ASSOC);
+                            
+                            // Récupération des utilisateurs qui ont liké l'article
+                            $liste_utilisateur_like = get_liste_likedislike($row["id_article"], "like");
+                            // Récupération des utilisateurs qui ont disliké l'article
+                            $liste_utilisateur_dislike = get_liste_likedislike($row["id_article"], "dislike");
+                            // Récupération du nombre de like
+                            $nblike = get_nb_likedislike($row["id_article"], "like");
+                            // Récupération du nombre de dislike
+                            $nbdislike = get_nb_likedislike($row["id_article"], "dislike");
                             $article = array(
                                 "id_article" => $row["id_article"],
                                 "date_d" => $row["date_d"],
                                 "contenu" => $row["contenu"],
-                                "nom" => $row["nom"],
-                                "prenom" => $row["prenom"]
+                                "nom" => $auteur["nom"],
+                                "prenom" => $auteur["prenom"],
+                                "nb_like" => $nblike,
+                                "nb_dislike" => $nbdislike,
+                                "listelike" => $liste_utilisateur_like,
+                                "listedislike" => $liste_utilisateur_dislike
                             );
-                        case "publisher":
-                            break;
+                            array_push($articles, $article);
+                        }
+                        // Encodage du tableau en JSON et renvoi de la réponse
+                        $push = json_encode($articles);
+                        deliver_response(200,"donnée transmise",$push);
                     }
+                    else{
+                        deliver_response(400,"critere demande",NULL);
+                    }
+
+
+                case "publisher":
+                    if(empty($_GET['action'])){
+                        // Requête SQL pour récupérer les articles avec leur ID, date, nom et prénom de l'auteur, et contenu
+                        $sql = "SELECT * FROM article";
+                        $psql = $database->prepare($sql);
+                        $psql->execute();
+                        // Exécution de la requête
+                        $result = $psql;
+
+                        // Création d'un tableau pour stocker les articles
+                        $articles = array();
+                        // Récupération des données et ajout au tableau des articles
+                        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                            echo $row["fk_id_auteur"];
+                            $reqauteur = "SELECT * FROM utilisateur WHERE id_utilisateur = :id_utilisateur";
+                            $auteur = $database->prepare($reqauteur);
+                            $params = array(
+                                ':id_utilisateur' => $row["fk_id_auteur"]
+                            );
+                            $auteur->execute($params);
+                            $auteur = $auteur->fetch(PDO::FETCH_ASSOC);
+                            
+                            // Récupération du nombre de like
+                            $nblike = get_nb_likedislike($row["id_article"], "like");
+                            // Récupération du nombre de dislike
+                            $nbdislike = get_nb_likedislike($row["id_article"], "dislike");
+                            $article = array(
+                                "id_article" => $row["id_article"],
+                                "date_d" => $row["date_d"],
+                                "contenu" => $row["contenu"],
+                                "nom" => $auteur["nom"],
+                                "prenom" => $auteur["prenom"],
+                                "nb_like" => $nblike,
+                                "nb_dislike" => $nbdislike
+                            );
+                            array_push($articles, $article);
+                        }
+                        // Encodage du tableau en JSON et renvoi de la réponse
+                        $push = json_encode($articles);
+                        deliver_response(200,"donnée transmise",$push);
                     
-                    array_push($articles, $article);
-                }
-                // Encodage du tableau en JSON et renvoi de la réponse
-                $push = json_encode($articles);
-                echo $push;
-            }
-            else{
-                deliver_response(400, "Vous n'êtes pas connecté", NULL);
-                break;
+                    }
+                    else if($_GET['action'] == "myarticles"){
+                        // Requête SQL pour récupérer les articles avec leur ID, date, nom et prénom de l'auteur, et contenu
+                        $sql = "SELECT * FROM article WHERE fk_id_auteur = :fk_id_auteur";
+                        $psql = $database->prepare($sql);
+                        $params = array(
+                            ':fk_id_auteur' => $payload_id
+                        );
+                        $psql->execute($params);
+                        $result = $psql;
+
+                        // Création d'un tableau pour stocker les articles
+                        $articles = array();
+                        // Récupération des données et ajout au tableau des articles
+                        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                            echo $row["fk_id_auteur"];
+                            $reqauteur = "SELECT * FROM utilisateur WHERE id_utilisateur = :id_utilisateur";
+                            $auteur = $database->prepare($reqauteur);
+                            $params = array(
+                                ':id_utilisateur' => $row["fk_id_auteur"]
+                            );
+                            $auteur->execute($params);
+                            $auteur = $auteur->fetch(PDO::FETCH_ASSOC);
+                            
+                            // Récupération des utilisateurs qui ont liké l'article
+                            $liste_utilisateur_like = get_liste_likedislike($row["id_article"], "like");
+                            // Récupération des utilisateurs qui ont disliké l'article
+                            $liste_utilisateur_dislike = get_liste_likedislike($row["id_article"], "dislike");
+                            // Récupération du nombre de like
+                            $nblike = get_nb_likedislike($row["id_article"], "like");
+                            // Récupération du nombre de dislike
+                            $nbdislike = get_nb_likedislike($row["id_article"], "dislike");
+                            $article = array(
+                                "id_article" => $row["id_article"],
+                                "date_d" => $row["date_d"],
+                                "contenu" => $row["contenu"],
+                                "nom" => $auteur["nom"],
+                                "prenom" => $auteur["prenom"],
+                                "nb_like" => $nblike,
+                                "nb_dislike" => $nbdislike,
+                                "listelike" => $liste_utilisateur_like,
+                                "listedislike" => $liste_utilisateur_dislike
+                            );
+                            array_push($articles, $article);
+                        }
+                        // Encodage du tableau en JSON et renvoi de la réponse
+                        $push = json_encode($articles);
+                        deliver_response(200,"Vos propres articles transmis",$push);
+                    }
+                    else {
+                        deliver_response(400,"Si une action est précisé elle doit être : myarticles",NULL);
+                        }
+                    
+                    break;
+                default:
+                    break;
             }
         }
         else{
-            deliver_response(400, "Vous n'êtes pas connecté", NULL);
-            break;
+            $sql = "SELECT id_article,date_d, contenu FROM article";
+            $psql = $database->prepare($sql);
+            $psql->execute();
+            $result = $psql->fetchAll(PDO::FETCH_ASSOC);
+            $push = json_encode($result);
+            deliver_response(200,"donnée transmise2",$push);
         }
-        
-        /// Envoi de la réponse au Client
-        deliver_response(200, "Votre message", $push);
         break;
 
     /// Cas de la méthode POST
@@ -299,5 +414,30 @@ function deliver_response($status, $status_message, $data){
     $json_response = json_encode($response);
     echo $json_response;
 }
+// Récupérer le nombre de like ou de dislike d'un article
+function get_nb_likedislike($id_article, $action){
+    global $database;
+    $sql = "SELECT COUNT(*) AS nb FROM likedislike WHERE fk_id_article = :fk_id_article AND action_a = :action_a";
+    $psql = $database->prepare($sql);
+    $params = array(
+        ':fk_id_article' => $id_article,
+        ':action_a' => $action
+    );
+    $psql->execute($params);
+    $result = $psql->fetch(PDO::FETCH_ASSOC);
+    return $result['nb'];
+}
+// Récupérer la liste des utilisateurs ayant liké ou disliké un article
+function get_liste_likedislike($id_article, $action){
+    global $database;
+    $sql = "SELECT nom, prenom, id_utilisateur FROM utilisateur WHERE id_utilisateur in (SELECT fk_id_utilisateur FROM likedislike WHERE fk_id_article = :fk_id_article AND action_a = :action_a)";
+    $psql = $database->prepare($sql);
+    $params = array(
+        ':fk_id_article' => $id_article,
+        ':action_a' => $action
+    );
+    $psql->execute($params);
+    $result = $psql->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
+}
 ?>
-
